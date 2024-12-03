881db39341f74790def9884f4aef73f70d144deb
Niko Zhang <hiniko23@gmail.com>
2024-12-02 22:16:34 -0500

    mfdca complete

--- dca_retrieval.py
+++ dca_retrieval.py
@@ -1,4 +1,5 @@
 from dca.dca_class import dca
+import pandas as pd
 import os
 import sys
 import pickle
@@ -8,10 +9,10 @@ def main():
     sequence_file = sys.argv[1]
     output_filename = sys.argv[2]
     protein_family = dca(sequence_file)
-    
-    with open(output_filename, 'wb') as f:
-    	pickle.dump(protein_family, f)
 
+    df = pd.DataFrame(protein_family.DI)
+    df.rename(columns={0: 'first_site', 1: 'second_site', 2: 'mf_dca'}, inplace=True)
+    df.to_csv(output_filename+'.csv')
 
 if __name__ == "__main__":
     main()
