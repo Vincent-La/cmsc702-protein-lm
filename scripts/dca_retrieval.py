4814384ffe41683c2f0cfdd8cdbe2cca330509a7
Niko Zhang <hiniko23@gmail.com>
2024-12-12 12:29:34 -0500

    implemented arg parser and fixed major bug

--- scripts/dca_retrieval.py
+++ scripts/dca_retrieval.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import sys
 import pickle
+import argparse
 
 # This script takes in a fasta file as input (arg 1) and the output file name
 # (arg 2) and returns a csv file containing the mfdcas.
@@ -12,11 +13,28 @@ import pickle
 # and create the corresponding fasta file. Then use the new fasta file containing
 # 2000 sequences as input for this scrpt to generate the .csv file.
 def main():
-    # DCA script
-    sequence_file = sys.argv[1]
-    output_filename = sys.argv[2]
-    protein_family = dca(sequence_file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--sequence_file',
+        type=str,
+        required=True,
+        help="Path of MSA (fasta format) file to compute mfdca",
+    )
 
+    parser.add_argument(
+        '--output_filename',
+        type=str,
+        required=True,
+        help="Path of output (csv format) file",
+    )
+    args = parser.parse_args()
+    
+    # DCA script
+    sequence_file = args.sequence_file
+    output_filename = args.output_filename
+    protein_family = dca(sequence_file)
+    protein_family.mean_field()
+    
     df = pd.DataFrame(protein_family.DI)
     df.rename(columns={0: 'first_site', 1: 'second_site', 2: 'mf_dca'}, inplace=True)
     df['first_site'] = df['first_site'].astype(int)
