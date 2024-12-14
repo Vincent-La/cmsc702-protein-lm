2b2aab182a3826ec1386674ec47353637bb4eaa1
Niko Zhang <hiniko23@gmail.com>
2024-12-14 17:40:35 -0500

    added cli command args

--- scripts/fasta_conversion.py
+++ scripts/fasta_conversion.py
@@ -1,5 +1,6 @@
 from Bio import AlignIO
 from Bio import SeqIO
+import argparse
 import sys
 
 
@@ -8,8 +9,25 @@ import sys
 # arg1 -> stockhom file path
 # arg2 -> path of file output
 def main():
-    file_path = sys.argv[1]
-    output_name = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--file_path',
+        type=str,
+        required=True,
+        help="stockhom file path to convert to fasta",
+    )
+
+    parser.add_argument(
+        '--output_path',
+        type=str,
+        required=True,
+        help="path of fasta file output",
+    )
+
+    args = parser.parse_args()
+    
+    file_path = args.file_path
+    output_name = args.output_path
     try:
         alignment = AlignIO.read(file_path, 'stockholm')
     except:
--- scripts/random_subset_fasta.py
+++ scripts/random_subset_fasta.py
@@ -2,25 +2,99 @@ from Bio import AlignIO
 from Bio import SeqIO
 import random
 import sys
+import argparse
+import numpy as np
+from difflib import SequenceMatcher
 
 # This script takes a random subset of a fasta file and
 # creates a new fasta file containing the subset.
 # arg1 -> the path of the fasta file to get the subset from
 # arg2 -> the number of sequences for the new fasta file
-# arg3 -> the path of the new fasta file
+# arg3 -> the left index for bounding the range of sites
+# arg4 -> the right index for bounding the range of sites
+# arg5 -> the path of the new fasta file
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+    
 def main():
-    file_path = sys.argv[1]
-    N = int(sys.argv[2])
-    output_name = sys.argv[3]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--file_path',
+        type=str,
+        required=True,
+        help="the path of the fasta file to get the subset from",
+    )
+
+    parser.add_argument(
+        '--N',
+        type=int,
+        required=True,
+        help="the number of sequences for the new fasta file",
+    )
+
+    parser.add_argument(
+        '--left',
+        type=int,
+        required=False,
+        help="the number of sequences for the new fasta file",
+    )
+
+    parser.add_argument(
+        '--right',
+        type=int,
+        required=False,
+        help="the number of sequences for the new fasta file",
+    )
+
+    parser.add_argument(
+        '--output_path',
+        type=str,
+        required=True,
+        help="the path of the new fasta file",
+    )
+    
+    args = parser.parse_args()
+    
+    file_path = args.file_path
+    N = args.N
+    L = args.left
+    R = args.right
+    output_name = args.output_path
     try:
-        sequences = list(SeqIO.parse(file_path, "fasta"))
+        seen = set()
+        records = []
+
+        flag = True
+        if L==None or R==None:
+            flag = False
+        for record in SeqIO.parse(file_path, 'fasta'): 
+            if flag and record[L:R].seq not in seen:
+                seen.add(record[L:R].seq)
+                records.append(record[L:R])
+            elif record.seq not in seen:
+                seen.add(record.seq)
+                records.append(record)
+        
+        dp = [[-1 for _ in range(len(records))] for _ in range(len(records))]
+        for i in range(len(records)):
+            for j in range(len(records)):
+                # get similarity between site i and site j
+                if dp[j][i] != -1:
+                    dp[i][j] == dp[j][i]
+                elif i != j:
+                    a = records[i]
+                    b = records[j]
+                    dp[i][j] = similar(a,b)
+                else:
+                    dp[i][j] = 1
+        
+                    
+                
     except:
         print('could not parse file -> not in fasta format')
 
-    random.shuffle(sequences)
-    print('Original number of sequences: ', len(sequences))
-    print('New number of sequences: ', N)
-    SeqIO.write(sequences[0:N], output_name, "fasta")
+    random.shuffle(records)
+    SeqIO.write(records[0:N], output_name, "fasta")
 
 if __name__ == "__main__":
     main()
